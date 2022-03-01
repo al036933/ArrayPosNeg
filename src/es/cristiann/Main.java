ead856785f106b1f5ca7faad99ccc1c34233b8a8
Cristian García Hernández <al036933@ieslaencanta.com>
2022-03-01 12:28:01 +0100

    Add 2 hecho

---
+++ src/es/cristiann/Main.java
@@ -0,0 +1,32 @@
+package es.cristiann;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] numeros = new int[5];
+        int[] otrosNumeros = new int[5];
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Te voy a pedir 5 números enteros: ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = entrada.nextInt();
+        }
+        System.out.println(" ");
+        for (int i = 0; i < otrosNumeros.length; i++) {
+            otrosNumeros[i] = numeros[i] * (-1);
+        }
+        System.out.println("Array 1: ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Array 2: ");
+        for (int i = 0; i < otrosNumeros.length; i++) {
+            System.out.print(otrosNumeros[i] + " ");
+        }
+        entrada.close();
+    }
+}
